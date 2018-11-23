452bb14afc8d62c8e0036a1f5e9a6bad1a924e0a
LeoCenturion <centurionm.leo@gmail.com>
2018-11-23 20:16:52 -0300

    parte 3 terminada

--- kern/env.c
+++ kern/env.c
@@ -557,7 +557,7 @@ env_run(struct Env *e)
 	curenv->env_status = ENV_RUNNING;
 	curenv->env_runs++;
 
-	//unlock_kernel();
+	unlock_kernel();
 
 	lcr3(PADDR(curenv->env_pgdir));
 	env_pop_tf(&curenv->env_tf);
