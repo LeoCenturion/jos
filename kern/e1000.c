2a8fae61dbb5ab0e96df9dedc78b3e308d9809a3
LeoCenturion <centurionm.leo@gmail.com>
2019-01-12 21:50:00 -0300

    pci_attach listo

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,3 +1,7 @@
 #include <kern/e1000.h>
 
 // LAB 6: Your driver code here
+int attach_e1000(struct pci_func *pcif){
+	pci_func_enable(pcif);
+	return 0;
+}
--- kern/e1000.h
+++ kern/e1000.h
@@ -1,4 +1,5 @@
 #ifndef JOS_KERN_E1000_H
 #define JOS_KERN_E1000_H
-
+#include<kern/pci.h>
 #endif  // JOS_KERN_E1000_H
+int attach_e1000(struct pci_func *pcif);
