708b27576964b31500580345e716ee9ff365b87a
LeoCenturion <centurionm.leo@gmail.com>
2019-01-14 22:11:52 -0300

    ex 4 parcialmente terminado

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,7 +1,31 @@
 #include <kern/e1000.h>
-
+#include <inc/stdio.h>
+#include <kern/pmap.h>
 // LAB 6: Your driver code here
+volatile uint32_t *bar0_addr;
+struct pci_func *e100_pci_func;
+volatile struct tx_desc trans_descr_list[E1000_TDL_SIZE] __attribute__ ((aligned (16)));
 int attach_e1000(struct pci_func *pcif){
+	e100_pci_func = pcif;
 	pci_func_enable(pcif);
+	bar0_addr = mmio_map_region(pcif->reg_base[0],pcif->reg_size[0]);
+	 cprintf("reg_base = %x \n reg_size = %d \n map = %x \n", 
+	 	pcif->reg_base[0], pcif->reg_size[0], bar0_addr); 
+	 uint32_t *status_reg = (void *)((uint32_t )bar0_addr + (uint32_t)0x8 ); 
+	 cprintf("status_reg = %x \n", *status_reg); 
+ 	*(uint32_t*)(bar0_addr + E1000_TDBAL) = (uint32_t)&trans_descr_list[0]; 
+	cprintf("tdbal = %p \n", *(uint32_t*)(bar0_addr + E1000_TDBAL)); 
+
+ 	*(uint32_t*)(bar0_addr + E1000_TDLEN) = (uint32_t)E1000_TDL_SIZE*sizeof(struct tx_desc); 
+ 	if( *(uint32_t*)(bar0_addr + E1000_TDLEN) & 0xFF)  
+ 		panic("TDLEN unaligned\n"); 
+
+ 	if( *(uint32_t*)(bar0_addr + E1000_TDBAL) & 0xF)  
+ 		panic("TDBAL unaligned\n"); 
+
+	 *(bar0_addr + E1000_TDH) = 0; 
+	 *(bar0_addr + E1000_TDT) = 0;
+	 *(bar0_addr + E1000_TCTL) = E1000_TCTL_EN | E1000_TCTL_PSP | E1000_TCTL_COLD_FULL_DUPLEX;
+	 *(bar0_addr + E1000_TIPG) = E1000_TIPG_IEEE;
 	return 0;
 }
--- kern/e1000.h
+++ kern/e1000.h
@@ -2,4 +2,48 @@
 #define JOS_KERN_E1000_H
 #include<kern/pci.h>
 #endif  // JOS_KERN_E1000_H
+#define E1000_TDL_SIZE 64       /* Transmit descriptor list length in descriptors */
+#define E1000_TDBAL    0x03800  /* TX Descriptor Base Address Low - RW */
+#define E1000_TDBAH    0x03804  /* TX Descriptor Base Address High - RW */
+#define E1000_TDLEN    0x03808  /* TX Descriptor Length - RW */
+#define E1000_TDH      0x03810  /* TX Descriptor Head - RW */
+#define E1000_TDT      0x03818  /* TX Descripotr Tail - RW */
+#define E1000_TCTL     0x00400  /* TX Control - RW */
+/* Transmit Control */
+#define E1000_TCTL_RST    0x00000001    /* software reset */
+#define E1000_TCTL_EN     0x00000002    /* enable tx */
+#define E1000_TCTL_BCE    0x00000004    /* busy check enable */
+#define E1000_TCTL_PSP    0x00000008    /* pad short packets */
+#define E1000_TCTL_CT     0x00000ff0    /* collision threshold */
+#define E1000_TCTL_COLD   0x003ff000    /* collision distance */
+#define E1000_TCTL_SWXOFF 0x00400000    /* SW Xoff transmission */
+#define E1000_TCTL_PBE    0x00800000    /* Packet Burst Enable */
+#define E1000_TCTL_RTLC   0x01000000    /* Re-transmit on late collision */
+#define E1000_TCTL_NRTU   0x02000000    /* No Re-transmit on underrun */
+#define E1000_TCTL_MULR   0x10000000    /* Multiple request support */
+#define E1000_TCTL_COLD_FULL_DUPLEX 0x00040000
+#define E1000_TIPG     0x00410  /* TX Inter-packet gap -RW */
+#define E1000_TIPG_IEEE 0x0060100a
 int attach_e1000(struct pci_func *pcif);
+
+struct tx_desc
+{
+	uint64_t addr;
+	uint16_t length;
+	uint8_t cso;
+	uint8_t cmd;
+	uint8_t status;
+	uint8_t css;
+	uint16_t special;
+}__attribute__((packed,aligned(16)));
+	
+/* Receive Descriptor */
+struct e1000_rx_desc {
+    uint64_t buffer_addr; /* Address of the descriptor's data buffer */
+    uint16_t length;     /* Length of data DMAed into data buffer */
+    uint16_t csum;       /* Packet checksum */
+    uint8_t status;      /* Descriptor status */
+    uint8_t errors;      /* Descriptor Errors */
+    uint16_t special;
+} __attribute__((packed,aligned(16)));
+
